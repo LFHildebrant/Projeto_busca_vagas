b681fb1d22d3bd9510bc5c589c1f36d49ea004a3
Luiz Felipe Hildebrant <luiz.felipe@webfloat.com.br>
2025-11-12 16:33:08 -0300

    Connection page

---
+++ busca_vagas_cliente/js/connection.js
@@ -0,0 +1,31 @@
+import { Config } from "./main.js";
+
+class Connection {
+    constructor(){
+        this.ip = document.getElementById("ip");
+        this.porta = document.getElementById("porta");
+        this.formConnection = document.getElementById("connection");
+        this.message = document.getElementById("message");
+
+        this.formConnection.addEventListener("submit", this.handleSubmitUpdate.bind(this));
+
+    }
+    handleSubmitUpdate(event){
+        event.preventDefault();
+        let url = "http://" + this.ip.value + ":" + this.porta.value;
+        Config.BASE_URL = url;
+        console.log(Config.BASE_URL);
+
+        setTimeout(() => {
+            if (Config.BASE_URL != null){
+                window.location.href = "index.html";
+            } else {
+                this.messageEl.style.color = "red";
+                this.messageEl.textContent = "Erro no form";
+            }
+        }, 1000)
+
+    }
+}
+
+new Connection();
